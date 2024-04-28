22fba3f09e82b7cad227d752a256ef0a61fcc98a
00Silent00 <ssmariseth@gmail.com>
2024-04-28 16:01:18 +0200

    first

---
+++ Assets/Scripts/Pmovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pmovement : MonoBehaviour
+{
+    public Rigidbody RB;
+    public float speed = 2;
+    float HorizontalSpeed;
+    float VerticalSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
