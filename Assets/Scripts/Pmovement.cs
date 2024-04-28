4a872e1da7a07779f5e13141e014c5de3abe6e8a
00Silent00 <ssmariseth@gmail.com>
2024-04-28 17:17:25 +0200

    basic player movement

--- Assets/Scripts/Pmovement.cs
+++ Assets/Scripts/Pmovement.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+
 
 public class Pmovement : MonoBehaviour
 {
     public Rigidbody RB;
     public float speed = 2;
-    float HorizontalSpeed;
-    float VerticalSpeed;
+    public float HorizontalSpeed;
+    public float VerticalSpeed;
+    public bool IsOnGround = true;
+    public float GravityMod;
     // Start is called before the first frame update
     void Start()
     {
+        Physics.gravity *= GravityMod;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            RB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+        HorizontalSpeed = Input.GetAxis("Horizontal");
+        VerticalSpeed = Input.GetAxis("Vertical");
+        RB.AddForce(Vector3.right * HorizontalSpeed * Time.fixedDeltaTime * speed, ForceMode.VelocityChange);
+        RB.AddForce(Vector3.forward * VerticalSpeed * Time.fixedDeltaTime * speed,  ForceMode.VelocityChange);
+        RB.drag = IsOnGround ? 15 : 0.01f;
+
+        if (IsOnGround == false)
+        {
+            speed = 0;
+        }else
+        {
+            speed = 10;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround = true;
     }
 }
