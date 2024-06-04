ec4757769f29e114c5072b1b2d7b32340bc0b99e
00Silent00 <ssmariseth@gmail.com>
2024-06-04 15:11:32 +0200

    wtf

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class spin : MonoBehaviour
 {
     public float spinSpeed = 10;
+    private Rigidbody ObsticalRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ObsticalRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
     }
 }
