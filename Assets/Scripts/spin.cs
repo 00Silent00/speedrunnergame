eabb65d04235b82d630ed1f994e4f3804bd4a244
00Silent00 <ssmariseth@gmail.com>
2024-06-12 19:41:58 +0200

    AAAAHHHHHH

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class spin : MonoBehaviour
 {
     public float spinSpeed = 10;
     private Rigidbody ObsticalRB;
+    Vector3 EulerAngleVelocity;
     // Start is called before the first frame update
     void Start()
     {
         ObsticalRB = GetComponent<Rigidbody>();
+
+        EulerAngleVelocity = new Vector3(0, spinSpeed, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    void fixeUpdateUpdate()
     {
-        ObsticalRB.AddTorque(Vector3.forward * spinSpeed * Time.deltaTime);
+          Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Time.fixedDeltaTime);
+
+          ObsticalRB.MoveRotation(ObsticalRB.rotation * deltaRotation);
+       
+         
+       
+       
     }
 }
